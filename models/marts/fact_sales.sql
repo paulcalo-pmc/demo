8d00b4962d4c03723a5d06f14244cf05964bc681
mrcalo@hotmail.com <mrcalo@hotmail.com>
2025-11-19 16:23:47 +0000

    latest change

--- models/marts/fact_sales.sql
+++ models/marts/fact_sales.sql
@@ -3,16 +3,39 @@
     tags=['mart', 'fact']
 ) }}
 
--- Keep only January 2000 data so the fact model runs fast
-with sales as (
-    select *
-    from {{ ref('stg_tpcds_store_sales') }}
-    where date_key between 20000101 and 20000131
+-- Find the first real month in the dataset
+with valid_dates as (
+    select
+        d_date_sk as date_key,
+        d_date as date_actual,
+        d_year,
+        d_moy,
+        row_number() over (order by d_date) as rn
+    from {{ source('tpcds', 'DATE_DIM') }}
+    where d_date_sk in (select distinct ss_sold_date_sk from {{ source('tpcds', 'STORE_SALES') }})
+),
+
+first_month as (
+    -- pick the earliest month with sales
+    select
+        d_year,
+        d_moy
+    from valid_dates
+    where rn = 1
+),
+
+sales as (
+    select ss.*
+    from {{ ref('stg_tpcds_store_sales') }} ss
+    join valid_dates vd
+      on ss.date_key = vd.date_key
+    join first_month fm
+      on vd.d_year = fm.d_year
+     and vd.d_moy = fm.d_moy
 ),
 
 items as (
-    select *
-    from {{ ref('dim_item') }}
+    select * from {{ ref('dim_item') }}
 )
 
 select
@@ -25,4 +48,4 @@ select
     i.category
 from sales s
 left join items i
-    on s.item_id = i.item_id
+  on s.item_id = i.item_id
